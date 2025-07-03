5c4caa0c2ca81462cd352adf0808b1c0fcb7c469
Althaf1166 <132194821+Althafahmed22@users.noreply.github.com>
2025-07-03 14:27:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,7 +49,7 @@ resource "aws_iam_role_policy_attachment" "ecs_task_policy_attach" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-resource "aws_iam_role" "ecs_task_execution_role" {
+data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
@@ -74,7 +74,7 @@ resource "aws_ecs_task_definition" "python_task" {
   network_mode            = "awsvpc"
   cpu                     = "256"
   memory                  = "512"
-  execution_role_arn      = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn      = data.aws_iam_role.ecs_task_execution_role.arn
   container_definitions   = jsonencode([
     {
       name      = "python-app"
